22ce16b9ebdac6d29210c161cc90703d9245007b
makhim-lee <yjsm2818@gmail.com>
2023-06-07 01:00:24 +0900

    test_example_v1.0.2

--- hand_tracking/hand_tracking.py
+++ hand_tracking/hand_tracking.py
@@ -1,63 +1,72 @@
 import cv2
-import mediapipe as mp
-
 import time
-import queue
-import threading
-Queue_frame = queue.Queue()
-
-exitFlag = True
-
-cap = cv2.VideoCapture(-1)
-success, img = cap.read()
-
-def show_window_thread():
-    global exitFlag
-    while exitFlag:
-        #if not Queue_frame.empty():
-        cv2.imshow("Gotcha", img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            exitFlag = False
-            break
-
-
-show_window = threading.Thread(target=show_window_thread)
-show_window.start()
-
-
-mp_drawing = mp.solutions.drawing_utils # 손 위에 그림을 그릴 수 있는 메소드
-mp_hands = mp.solutions.hands # MediaPipe solution에서 제공하는 손 모델
-
-hands = mp_hands.Hands(
-    max_num_hands = 1, # 인식할 손모양의 갯수, 생략하면 2가 지정된다.
-    min_detection_confidence = 0.5, # 성공적인 것으로 간주되는 최소 신뢰도 값. 0.0 ~1.0사이로서 기본값은 0.5이다.
-    min_tracking_confidence = 0.5) # 손 랜드마크가 성공적으로 추적된 것으로 간주되는 최소 신뢰도 값. 0.0 ~1.0 사이로서 기본값은 0.5이다. 이 값을 높이면 시간이 더 소요되지만 좀 더 정확한 작동이 보장된다. 
-
-while exitFlag:
-	if not success:
-		continue 
-	# OpenCV 영상은 BGR 형식인데 MediaPipe에서는 RGB 형식을 사용하므로 영상형식을 변환해 준다.
-	imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-	# MediaPipe의 hands 모듈을 이용해서 손동작을 인식한다. 손동작 인식 AI모델이 작동되고 결과 값이 result로 저장된다. 
-	results = hands.process(imgRGB)
-	# MediaPipe용 RGB 형식으로 변환했던 것을 OpenCV 영상처리를 위해 다시 BGR형식으로 되돌린다. 
-	imgRGB = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-	# result값이 정상인 경우에만 후속 작업 처리한다. 
-	if results.multi_hand_landmarks:
-		# 실제 손이 인식될 때 점(랜드마크)을 찍는다.
-		for handLms in results.multi_hand_landmarks:
-			# results로 반환된 landmark 데이터를 사용한다. 인식된 손가락 모양은 index값을 가지는 배열로 제공된다.
-			for id, lm in enumerate(handLms.landmark):
-				h, w, c = img.shape
-				cx, cy = int(lm.x*w), int(lm.y*h)
-				print(id, " :" , cx, cy)
-				if id == 0:
-					cv2.circle(img, (cx,cy), 20, (255,0,0), cv2.FILLED)
-        	# MediaPipe에 내장된 유틸리티 기능을 이용해서 구해진 손가락 모양을 서로 연결한 그림을 그려준다.
-			mp_drawing.draw_landmarks(img, handLms, mp_hands.HAND_CONNECTIONS)
-			#if not Queue_frame.full():
-  			#	Queue_frame.put(img)
-	
-  
-show_window.join()
-cv2.destroyAllWindows()
+import numpy as np
+import hand_detector as hd
+import pyautogui
+ 
+ 
+wCam, hCam = 640, 480
+frameR = 100
+smoothening = 7
+ 
+pTime = 0
+plocX, plocY = 0, 0
+clocX, clocY = 0, 0
+ 
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+detector = hd.handDetector(detectionCon=0.7)
+wScr, hScr = pyautogui.size()
+print(wScr, hScr)
+ 
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList, bbox = detector.findPosition(img)
+    output = img.copy()
+ 
+    if len(lmList) != 0:
+        # print(lmList[4], lmList[8])
+        x1, y1 = lmList[8][1:]
+        x2, y2 = lmList[12][1:]
+ 
+        fingers = detector.fingersUp()
+        # print(fingers)
+        cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR), (205, 250, 255), -1)
+        img = cv2.addWeighted(img, 0.5, output, 1 - .5, 0, output)
+ 
+        # Only Index Finger : Moving Mode
+        if fingers[1] == 1 and fingers[2] == 0:
+            # Convert Coordinates
+            x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
+            y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
+ 
+            # Smoothen Values
+            clocX = plocX + (x3 - plocX) / smoothening
+            clocY = plocY + (y3 - plocY) / smoothening
+ 
+            # Move Mouse
+            pyautogui.moveTo(wScr - clocX, clocY)
+            cv2.circle(img, (x1, y1), 6, (255, 28, 0), cv2.FILLED)
+            plocX, plocY = clocX, clocY
+            # cv2.putText(img, 'Moving Mode', (20, 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+ 
+        # Both Index and middle fingers are up : Clicking Mode
+        if fingers[1] == 1 and fingers[2] == 1:
+            # Find distance between fingers
+            length, img, lineInfo = detector.findDistance(8, 12, img)
+ 
+            # Click mouse if distance short
+            if length < 40:
+                cv2.circle(img, (lineInfo[4], lineInfo[5]), 6, (0, 255, 0), cv2.FILLED)
+                # cv2.putText(img, 'Click!!', (20, 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+                pyautogui.click()
+ 
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+ 
+    cv2.imshow("Vitual mouse monitor", cv2.flip(img, 1))
+    cv2.setWindowProperty("Vitual mouse monitor", cv2.WND_PROP_TOPMOST, 1)
+    cv2.waitKey(1)
