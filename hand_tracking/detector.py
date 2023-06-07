f41181308f397ad5e702a4e07b48d3d25cbbbe76
makhim-lee <yjsm2818@gmail.com>
2023-06-08 02:13:52 +0900

    start_mycode_v1.1.1

--- hand_tracking/detector.py
+++ hand_tracking/detector.py
@@ -1,49 +1,65 @@
 
 import math
 import cv2
-import mediapipe as mp
+
+from mediapipe.python.solutions import hands as mp_hands
+from mediapipe.python.solutions.drawing_utils import draw_landmarks
+
 import time
-class handDetector():
-    def __init__(self, mode=False, maxHands=1, detectionCon=0.5, trackCon=0.5): 
-        self.mpHands = mp.solutions.hands
-        self.mpDraw = mp.solutions.drawing_utils
-        
-        self.mode = mode
-        self.maxHands = maxHands
-        self.detectionCon = detectionCon
-        self.trackCon = trackCon
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
+class HandDetector():
+    def __init__(self, mode=False, max_num_hands=1, min_detection_confidence=0.5, min_tracking_confidence=0.5): 
+        self.hands = mp_hands.Hands(
+            mode, 
+            max_num_hands, 
+            min_detection_confidence, 
+            min_tracking_confidence)
          
-        self.tipIds = [4, 8, 12, 16, 20]
+        self.finger_id = [4, 8, 12, 16, 20]
  
-        self.lmList = []
+        #self.lm_list = []
         self.results = None
     
     def findHands(self, img):
         rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(rgb_img)
-        # print(results.multi_hand_landmarks)
  
         if self.results.multi_hand_landmarks:
-            for handLms in self.results.multi_hand_landmarks:
-                self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+            for hand_lms in self.results.multi_hand_landmarks:
+                draw_landmarks(img, hand_lms, mp_hands.HAND_CONNECTIONS)
         return img
     
-    def findPosition(self, img, handNo=0):
-        xList = []
-        yList = []
-        self.lmList = []
- 
+    def findLandmarks(self, img, hand_index=0):
+        self.lm_list = []
         if self.results.multi_hand_landmarks:
-            myHand = self.results.multi_hand_landmarks[handNo]
+            myHand = self.results.multi_hand_landmarks[hand_index]
  
             for id, lm in enumerate(myHand.landmark):
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                xList.append(cx)
-                yList.append(cy)
-                self.lmList.append([id, cx, cy])
- 
+                self.lm_list.append([id, cx, cy])
                 if id == 0:
                     cv2.circle(img, (cx, cy), 6, (0, 0, 255), cv2.FILLED)
-        return self.lmList
+        return self.lm_list
+    
+    def fingersCheck(self):
+        fingers = []
+        fingers.append(1 if self.lm_list[self.finger_id[0]][1] < self.lm_list[self.finger_id[0] - 2][1] \
+            else 0)
+        fingers = [1 if self.lm_list[self.finger_id[id]][2] < self.lm_list[self.finger_id[id] - 2][2] \
+            else 0 \
+                for id in range(1,5)]
+        
+        return fingers
+     
+    def calculateDistance(self, p1, p2, img):
+        x1, y1 = self.lm_list[p1][1:]
+        x2, y2 = self.lm_list[p2][1:]
+        
+        cv2.line(img, (x1, y1), (x2, y2), (0, 255, 255), 3)
+        cv2.circle(img, (x1, y1), 6, (0, 255, 255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 6, (0, 255, 255), cv2.FILLED)
+ 
+        length = math.hypot(x2 - x1, y2 - y1)
+ 
+        return length, img, [x1, y1, x2, y2]
+ 
--- hand_tracking/tracking.py
+++ hand_tracking/tracking.py
@@ -2,15 +2,23 @@ import cv2
 import time
 import numpy as np
 import detector
-
-
+from pprint import pprint
 cap = cv2.VideoCapture(-1)
-detector = detector.handDetector()
+detector = detector.HandDetector()
+prev_fingers= None
+
 
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
-    imList = detector.findPosition(img) 
+    
+    lm_list = detector.findLandmarks(img) 
+    if len(lm_list) != 0 :
+        fingers = detector.fingersCheck()
+        pprint(fingers)
+        if fingers[1] == 1 and fingers[2] == 1 :
+            length, img, linInfo = detector.calculateDistance(8, 12, img) 
+
     cv2.imshow("Gotcha", cv2.flip(img, 1))
     cv2.waitKey(1)
     
