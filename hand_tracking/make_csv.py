3e12986f2fb63d38dff694f9e448e3a1deb59d7b
makhim-lee <yjsm2818@gmail.com>
2023-06-08 22:23:37 +0900

    deeplearning_v1.2.1

--- hand_tracking/make_csv.py
+++ hand_tracking/make_csv.py
@@ -1,23 +1,53 @@
 import pandas as pd
 import cv2
-import time
 import numpy as np
 import detector
 from pprint import pprint
+import time
+
 cap = cv2.VideoCapture(-1)
 detector = detector.HandDetector()
-
+filename = "landmarks.csv"
+save_data=[]
+count = 0
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
-    
     lm_list,save_list = detector.findLandmarks(img) 
     if cv2.waitKey(1) & 0xFF == ord('p'):#(lm_list is not None):
-        df = pd.DataFrame(save_list)
-        print(df)
-        df.to_csv("landmarks.csv",header=None, index=None)   
+        save_data.append(save_list)
+        count += 1
+        time.sleep(1)
+        print(count)
     cv2.imshow("Gotcha", cv2.flip(img, 1))
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        pd_data = pd.DataFrame(save_data)
+        pd_data.to_csv(filename, mode='w')
         break
+
 cap.release()
 cv2.destroyAllWindows()
+
+dateset = pd.read_csv(filename, index_col=0)
+data = dateset.to_numpy()
+pprint(data)
+
+""" test append data
+double_lists = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10, 11, 12],
+]
+
+for _ in range(5):
+    save_data.append(double_lists)
+    
+pd_data = pd.DataFrame(save_data)
+pd_data.to_csv(filename, mode='w')# a ->덮어쓰기 x 추가 ㅇ
+
+dateset = pd.read_csv(filename, index_col=0)
+data = dateset.to_numpy()
+pprint(data)
+print(type(data)) 
+"""
