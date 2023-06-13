b3b32f75e51eda2694bfa76b89a430fe111c9123
makhim-lee <yjsm2818@gmail.com>
2023-06-13 17:17:18 +0900

    detec_qr_v.1.5.1

--- hand_tracking/make_csv.py
+++ hand_tracking/make_csv.py
@@ -7,21 +7,28 @@ import time
 
 cap = cv2.VideoCapture(-1)
 detector = HandDetector()
-filename = "landmarks.csv"
-save_data=[]
+filename = "test.csv"
+#filename = "landmarks.csv"
+save_datas=[]
 count = 0
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
-    lm_list,save_list = detector.findLandmarks(img) 
+    lm_list,save_data = detector.findLandmarks(img)
     if cv2.waitKey(1) & 0xFF == ord('p'):#(lm_list is not None):
-        save_data.append(save_list)
+        if count == 0 :
+            save_datas = save_data
+            print(save_datas.shape)
+        else :
+            print(save_data.shape)
+            save_datas = np.vstack((save_datas, save_data))
+        #save_datas.append(save_data)
         count += 1
         time.sleep(1)
         print(count)
     cv2.imshow("Gotcha", cv2.flip(img, 1))
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        pd_data = pd.DataFrame(save_data)
+        pd_data = pd.DataFrame(save_datas)
         pd_data.to_csv(filename, mode='w')
         break
 
@@ -41,9 +48,9 @@ double_lists = [
 ]
 
 for _ in range(5):
-    save_data.append(double_lists)
+    save_datas.append(double_lists)
     
-pd_data = pd.DataFrame(save_data)
+pd_data = pd.DataFrame(save_datas)
 pd_data.to_csv(filename, mode='w')# a ->덮어쓰기 x 추가 ㅇ
 
 dateset = pd.read_csv(filename, index_col=0)
