3e1814972da67af70d9b96d2898907910bafcc81
makhim-lee <yjsm2818@gmail.com>
2023-06-14 15:20:18 +0900

    last_test_v1.9.0

--- hand_tracking/detector.py
+++ hand_tracking/detector.py
@@ -23,14 +23,12 @@ class HandDetector():
         self.lm_list = []
         self.results = None
         
-        #self.model = tf.keras.models.load_model('hand_model.h5')
     def findHands(self, img):
         rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(rgb_img)
         if self.results.multi_hand_landmarks:
             for hand_lms in self.results.multi_hand_landmarks:
                 draw_landmarks(img, hand_lms, mp_hands.HAND_CONNECTIONS)
-        return img
     
     def findLandmarks(self, img, hand_index=0):
         self.lm_list = []
@@ -42,29 +40,28 @@ class HandDetector():
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 self.lm_list.append([id, cx, cy])
-                #self.input_model_data.append([round(lm.x, 3), round(lm.y, 3), round(lm.z, 3)])
-                input_model_data = np.append(input_model_data, [lm.x,lm.y,lm.z])
+                #input_model_data.append(round(lm.x, 3), round(lm.y, 3), round(lm.z, 3))
+                input_model_data.append(round(lm.x, 3))
+                input_model_data.append(round(lm.y, 3))
+                input_model_data.append(round(lm.z, 3))
+                #input_model_data = np.append(input_model_data, [lm.x,lm.y,lm.z])
                 if id == 0:
                     cv2.circle(img, (cx, cy), 6, (0, 0, 255), cv2.FILLED)
         #input_data = np.array(self.input_model_data).reshape(1,-1)
         #prediction = self.model.predict(input_data)
-        return self.lm_list, input_model_data
-'''
-    def detectorMotion(self):
-        prediction = []
-        #if self.input_model_data :
-        input_data = np.array(self.input_model_data).reshape(1,-1)
-        prediction = self.model.predict(input_data)
-        if prediction[0][0] > 0.9 :
-            return "Vectory"
-        elif prediction[0][1] > 0.98 :
-            return "OK"
-        elif prediction[0][2] > 0.9 :
-            return "Pointer"
-        else : 
-            return None
-'''
+        return input_model_data
+    
+    def pointerMouse(self, img):
+        pointer = []
+        if self.results.multi_hand_landmarks:
+            x = self.lm_list[8][1]
+            y = self.lm_list[8][2]
+            pointer = [x, y]
+            cv2.circle(img, (x, y), 6, (0, 255, 255), cv2.FILLED)
+        return pointer, img
 
+
+    
 def detectorMotion(queue_input, queue_output):
     model = tf.keras.models.load_model('hand_model.h5')
     flag = 0
@@ -76,61 +73,20 @@ def detectorMotion(queue_input, queue_output):
     while(True):
         if not queue_input.empty():
             item = queue_input.get()
-            if item is None:
-                break
             prediction = model.predict(np.array(item).reshape(1,-1))
-             
+            #prediction = model.predict(item)
             for index, (output_string, compare_flag) in dic_prediction.items():
                 if prediction[0][index] > 0.9 and flag != compare_flag:
                     queue_output.put(output_string)
                     flag = compare_flag
+                    print("input : ", output_string)
                     break
                 elif prediction[0][index] > 0.9 :
                     break
             else:
                 flag = 0
-            '''
-            
-            
-            if prediction[0][0] > 0.9 and not flag == 1:
-                queue_output.put("Vectory")
-                flag = 1
-            elif prediction[0][1] > 0.98 and not flag == 2:
-                queue_output.put("OK")
-                flag = 2
-            elif prediction[0][2] > 0.9 and not flag == 3:
-                queue_output.put("Pointer")
-                flag = 3
-            else:
-                flag = 0            
-            '''            
-
-
-        
         
  
 if __name__ == "__main__":
     print("OpenCV : ",cv2.__version__)
     print("tensorflow : ",tf.__version__)    
-    #def fingersCheck(self):
-    #    fingers = []
-    #    fingers.append(1 if self.lm_list[self.finger_id[0]][1] < self.lm_list[self.finger_id[0] - 2][1] \
-    #        else 0)
-    #    fingers = [1 if self.lm_list[self.finger_id[id]][2] < self.lm_list[self.finger_id[id] - 2][2] \
-    #        else 0 \
-    #            for id in range(1,5)]
-    #    
-    #    return fingers
-    # 
-    #def calculateDistance(self, p1, p2, img):
-    #    x1, y1 = self.lm_list[p1][1:]
-    #    x2, y2 = self.lm_list[p2][1:]
-    #    
-    #    cv2.line(img, (x1, y1), (x2, y2), (0, 255, 255), 3)
-    #    cv2.circle(img, (x1, y1), 6, (0, 255, 255), cv2.FILLED)
-    #    cv2.circle(img, (x2, y2), 6, (0, 255, 255), cv2.FILLED)
- #
-    #    length = math.hypot(x2 - x1, y2 - y1)
- #
-    #    return length, img, [x1, y1, x2, y2]
- 
