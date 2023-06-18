eb0de624e36db60b012af7a78a2ed54a23bc02cc
makhim-lee <yjsm2818@gmail.com>
2023-06-18 14:04:59 +0900

    V2.0.1

---
+++ test_file/test_tts.py
@@ -0,0 +1,12 @@
+from gtts import gTTS
+import os
+
+def speak(text):
+    tts = gTTS(text=text, lang='en')  # Convert text to speech
+    filename = "speech.mp3"  # Name of mp3 file
+    tts.save(filename)  # Save speech audio into a file
+    os.system("mpg321 " + filename)  # Play the mp3 file
+
+s = "Please write in English language."  # The string variable
+speak(s)  # Output the string variable to speaker
+
